26966441e2a7691c9972fe7ef32c905939cef612
david whyte <kingstonwhyte87@gmail.com>
2018-05-21 12:19:27 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from extensions import login_manager
+from extensions import db,migrate
+from extensions import ma,cors
+#blueprints
+
+from blueprints.user import user
+from blueprints.post import post
+
+#app setup
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+	return "hello"
+
+#app run
+if __name__=='__main__':
+    app.run()
